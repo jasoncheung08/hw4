31cbc82ce4ec6fb59be5c99798263b31fa7478d2
Ka Yiu Jason Cheung <cheungja@usc.edu>
2025-04-04 04:59:15 +0000

    first question done

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -9,10 +9,30 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+// Check if all leaf paths are equal
+bool checkPaths(Node* node, int depth, int& leafDepth) {
+    if (node == nullptr) {
+        return true;
+    }
+
+    // If it is a leaf
+    if (node->left == nullptr && node->right == nullptr) {
+        if (leafDepth == -1) {
+            leafDepth = depth;
+        }
+        return depth == leafDepth;
+    }
+
+    // Recursive check left and right subtrees
+    bool leftOK = checkPaths(node->left, depth + 1, leafDepth);
+    bool rightOK = checkPaths(node->right, depth + 1, leafDepth);
+    return leftOK && rightOK;
+}
+
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
-
+    int leafDepth = -1;
+    return checkPaths(root, 0, leafDepth);
 }
 
